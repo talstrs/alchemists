9bbc4ea88b590cea09bcff4398a8ebfb6bf6881d
gksqhfdl12 <gksqhfdl12@gmail.com>
2024-05-16 17:03:00 +0900

    2024 0516 - base status

---
+++ alchemists/src/main/java/com/alchemists/common/constants/Constants.java
@@ -0,0 +1,39 @@
+package com.alchemists.common.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class Constants {
+
+	// Date 타입 포맷 상수
+	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+
+	// Date 시작일 기본 설정을 위한 상수
+	public static final int DATE_INTERVAL = -30;
+
+	// 시간 기본 설정 상수
+	public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
+
+	// 페이징 처리 상수 - 화면에 보여줄 데이터 줄 갯수
+	public static final int ROW_NUM_TO_SHOW = 5;
+
+	// 페이징 처리 상수 - 화면에 보여줄 페이징 번호 갯수
+	public static final int PAGE_NUM_TO_SHOW = 5;
+
+	public static final int SESSION_MINUTE_XDM = 30;
+
+	public static final List<String> CHARACTER_NAMES = Arrays.asList("재키", "아야", "쇼우", "키아라", "시셀라", "실비아", "리 다이린",
+			"아드리아나", "피오라", "쇼이치", "엠마", "레녹스", "로지", "루크", "캐시", "아델라", "매그너스", "버니스", "바바라", "알렉스", "수아", "레온", "일레븐",
+			"자히르", "리오", "윌리엄", "니키", "나타폰", "얀", "이바", "다니엘", "나딘", "제니", "카밀로", "클로에", "요한", "비앙카", "셀린", "에키온", "현우",
+			"마이", "에이든", "라우라", "띠아", "펠릭스", "엘레나", "프리야", "하트", "아디나", "마커스", "칼라", "에스텔", "피올로", "마르티나", "헤이즈", "아이솔",
+			"아이작", "타지아", "이렘", "테오도르", "이안", "바냐", "데비", "마를렌", "데비마를렌", "리다이린", "아르다", "아비게일", "알론소", "레니", "츠바메",
+			"케네스", "카티야", "유키", "혜진"
+
+	);
+	
+	//로그인 인터셉터 기능 구현 시 필요한 로그인 화면 폼 URL
+	public static final String URL_XDMLOGINFORM = "/adminLoginRegister";
+	
+	
+	
+}
