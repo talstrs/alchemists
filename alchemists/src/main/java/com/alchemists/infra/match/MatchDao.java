3de67b3bed220e569362e10c66ec8a80c58d75d6
gksqhfdl12 <gksqhfdl12@gmail.com>
2024-05-21 16:18:11 +0900

    .2

--- alchemists/src/main/java/com/alchemists/infra/match/MatchDao.java
+++ alchemists/src/main/java/com/alchemists/infra/match/MatchDao.java
@@ -1,6 +1,30 @@
 package com.alchemists.infra.match;
 
+import java.util.List;
+
 public interface MatchDao {
-	
-	
+
+	// DTO 중 하나를 리턴
+	public MatchDto selectOne(MatchDto dto);
+
+	// 등록 버튼
+	public int insert(MatchDto dto);
+
+	// 수정 버튼
+	public int update(MatchDto dto);
+
+	// 삭제 버튼
+	public int uelete(MatchDto dto);
+
+	// 삭제 버튼(drop)
+	public int delete(MatchDto dto);
+
+	// 페이징처리없는 코드그룹 리스트 리턴
+	public List<MatchDto> selectListWithoutPaging();
+
+	// 페이징 처리 리스트
+	public List<MatchDto> selectListWithPaging(MatchVo vo);
+
+	// 개수 리턴
+	public Integer selectCount(MatchVo vo);
 }
--- alchemists/src/main/java/com/alchemists/infra/match/MatchService.java
+++ alchemists/src/main/java/com/alchemists/infra/match/MatchService.java
@@ -17,4 +17,48 @@ public class MatchService {
 	@Autowired
 	MatchDao matchDao;
 
+	// DTO 중 하나를 리턴
+	public MatchDto selectOne(MatchDto dto) {
+		return matchDao.selectOne(dto);
+
+	}
+
+	// 등록 버튼
+	public int insert(MatchDto dto) {
+		matchDao.insert(dto);
+		return 1;
+	}
+
+	// 수정 버튼
+	public int update(MatchDto dto) {
+		matchDao.update(dto);
+		return 1;
+	}
+
+	// 삭제 버튼
+	public int uelete(MatchDto dto) {
+		matchDao.uelete(dto);
+		return 1;
+	}
+
+	// 삭제 버튼(drop)
+	public int delete(MatchDto dto) {
+		matchDao.delete(dto);
+		return 1;
+	}
+
+	// 페이징처리없는 코드그룹 리스트 리턴
+	public List<MatchDto> selectListWithoutPaging() {
+		return matchDao.selectListWithoutPaging();
+	}
+
+	// 페이징 처리 리스트
+	public List<MatchDto> selectListWithPaging(MatchVo vo) {
+		return matchDao.selectListWithPaging(vo);
+	}
+
+	// 개수 리턴
+	public Integer selectCount(MatchVo vo) {
+		return matchDao.selectCount(vo);
+	}
 }
