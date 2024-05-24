ed8a8a33169dbcb39d65a663381c9fa56d2ac0f8
talstrs <tal95@daum.net>
2024-05-24 14:39:15 +0900

    usr assets 추가

--- alchemists/src/main/java/com/alchemists/common/constants/Constants.java
+++ alchemists/src/main/java/com/alchemists/common/constants/Constants.java
@@ -32,7 +32,7 @@ public final class Constants {
 	);
 	
 	//로그인 인터셉터 기능 구현 시 필요한 로그인 화면 폼 URL
-	public static final String URL_XDMLOGINFORM = "/adminLoginRegister";
+	public static final String URL_LOGINFORM = "/memberUsrLogin";
 	
 	
 	
